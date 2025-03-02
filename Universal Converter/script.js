b37c09ae54355c68b573baa205d9ba47b3f6552f
soumyabrata2006 <soumyabrata8974@gmail.com>
2025-03-02 20:35:11 +0530

    kelvin conversion

--- Universal Converter/script.js
+++ Universal Converter/script.js
@@ -1,8 +1,10 @@
+// convert
 document.getElementById('convert').addEventListener('click', () => {
 	input=document.getElementById('input').value
 	input_unit=document.getElementById('input-unit').value
 	output_unit=document.getElementById('output-unit').value
 	output=0
+	
 	// Celsius-Fahrenheit
 	if(input_unit=="celsius" && output_unit=="fahrenheit") {
 		output=(input*9/5)+32
@@ -10,6 +12,15 @@ document.getElementById('convert').addEventListener('click', () => {
 	}
 	else if(input_unit=="fahrenheit" && output_unit=="celsius") {
 		output=(input-32)*5/9
+		document.getElementById('output').value=output.toString()
+	}
+	// Celsius-Kelvin
+	else if(input_unit=="celsius" && output_unit=="kelvin") {
+		output=Number(input)+273.15
+		document.getElementById('output').value=output.toString()
+	}
+	else if(input_unit=="kelvin" && output_unit=="celsius") {
+		output=Number(input)-273.15
 		document.getElementById('output').value=output.toString()
 	}
 	// centimetres-metres
@@ -42,4 +53,12 @@ document.getElementById('convert').addEventListener('click', () => {
 	else {
 		alert("Invalid input and output units.")
 	}
+})
+
+// clear
+document.getElementById('clear').addEventListener('click', () => {
+	document.getElementById('input').value=''
+	document.getElementById('input-unit').value='select'
+	document.getElementById('output').value=''
+	document.getElementById('output-unit').value='select'
 })
