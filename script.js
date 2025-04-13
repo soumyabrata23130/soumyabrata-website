052cf387935555a27edc62c9f5969d5704476b83
soumyabrata2006 <soumyabrata8974@gmail.com>
2025-04-14 00:05:32 +0530

    common header and footer

--- script.js
+++
@@ -1,6 +0,0 @@
-// toggles dark and light modes
-document.getElementById("theme-select").addEventListener("change", () => {
-	document.body.classList.remove("dark")
-	if(this.value==="dark")
-		document.body.classList.add("dark")
-})
