f82236e3d6dbc5b91e845443a496a3f1019cc0fb
soumyabrata2006 <soumyabrata8974@gmail.com>
2025-07-11 12:20:23 +0530

    fix day-night issue with a new code

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Soumyabrata's Website
 
-My personal website built using HTML, CSS and JavaScript. It contains a portfolio to introduce myself and links to other projects and websites. It is designed to be as responsive as possible, so that it can be comfortably read in a mobile as well as in a PC.
+My personal website built using HTML, CSS and JavaScript. It contains a portfolio to introduce myself and links to other projects and websites. Inspired by the (https://github.com/SukritDeb/nubfolio)[Nubfolio] by Sukrit Deb, it is designed to be as responsive as possible, so that it can be comfortably read in a mobile as well as in a PC.
 
 Demo: https://soumyabrata23130.github.io/soumyabrata-website
 
 ## Known Issues
 
-- Manual dark-light switch is commented due to some issues.
 - The top section links don't appear in mobile mode.
 - Reloading the website always leads to the top instead of a given section.
--- theme-select.js
+++ theme-select.js
@@ -1,29 +1,34 @@
-<!--
-document.getElementById("theme-select").addEventListener("change", function () {
-	const theme=document.getElementById("theme-select").value
-	console.log("Theme: " + theme)
+const themeSelect = document.getElementById("theme-select")
 
-	switch(theme) {
-		case "dark": // if theme is dark, select dark theme
-			document.querySelector("html").style.cssText = `
-				color-scheme: dark;
-				--background: #0a0a0a;
-				--box: #202020;
-				--button: darkgreen;
-				--foreground: #ededed;
-			`
-			break
-		case "light": // if theme is light, select light theme
-			document.querySelector("html").style.cssText = `
-				color-scheme: light;
-				--background: #f0f0f2;
-				--box: white;
-				--button: lightgreen;
-				--foreground: black;
-			`
-			break
-		default: // otherwise, select system default
-			document.querySelector("html").style.cssText = ""
+function applyTheme(theme) {
+	if (theme === "dark" || theme === "light") {
+		document.documentElement.setAttribute("data-theme", theme)
 	}
+	else if (theme === "system") {
+		const systemDark = window.matchMedia("(prefers-color-scheme: dark)").matches
+		document.documentElement.setAttribute("data-theme", systemDark ? "dark" : "light")
+	}
+}
+
+function setTheme(theme) {
+	localStorage.setItem("theme-preference", theme)
+	applyTheme(theme)
+}
+
+function initTheme(theme) {
+	const savedTheme = localStorage.getItem("theme-preference") || "system"
+	themeSelect.value = savedTheme
+	applyTheme(savedTheme)
+
+	if (savedTheme === "system") {
+		window.matchMedia("(prefers-coloe-scheme: dark)").addEventListener("change", () => {
+			applyTheme("system")
+		})
+	}
+}
+
+themeSelect.addEventListener("change", (e) => {
+	setTheme(e.target.value)
 })
--->
+
+initTheme()
